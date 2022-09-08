d67d22b44f54605aac6c71417f0ed778138e79a6
Best Cat <kotova-irina11@rambler.ru>
2022-09-08 17:22:32 +0300

    Метод 2

---
+++ metod_2/Program.cs
@@ -0,0 +1,21 @@
+﻿// Метод 2 НЕ возвращает, принимает
+void Method2(string msg)
+{
+  Console.WriteLine(msg);  
+}
+//Вызов метода
+//Method2(msg: "Текст сообщения"); именованные аргументы
+//Method2("Текст сообщения");
+
+
+void Method21(string msg, int count)
+{
+  int i = 0;
+  while (i<count)
+  {
+    Console.WriteLine(msg); 
+    i++;
+  }
+}
+//Вызов метода
+Method21(msg: "Текст сообщения", count: 5);
