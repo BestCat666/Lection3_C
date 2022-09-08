292f18da8c1efb5332847712b4b3ed1c34c1ad04
Best Cat <kotova-irina11@rambler.ru>
2022-09-08 18:00:44 +0300

    FOR

---
+++ for/Program.cs
@@ -0,0 +1,14 @@
+﻿// Цикл FOR
+//Метод 4 принимает и возвращает
+string Method4(int count, string text)
+{
+string result = ""; // String.Empty;
+for ( int i = 0; i < count;  i++)
+{
+    result = result + text;
+   
+}
+return result;
+}
+string res = Method4(10, "кусь");
+Console.WriteLine(res);
