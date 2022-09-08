684d0e92e5367b58f7e5152272fb2047324a3a8c
Best Cat <kotova-irina11@rambler.ru>
2022-09-08 17:35:26 +0300

    Метод 4

---
+++ metod_4/Program.cs
@@ -0,0 +1,14 @@
+﻿// Метод 4 принимает и возвращает
+string Method4(int count, string text)
+{
+    int i = 0;
+    string result = ""; // String.Empty;
+while( i<count)
+{
+    result = result + text;
+    i++;
+}
+return result;
+}
+string res = Method4(10, "кусь");
+Console.WriteLine(res);
